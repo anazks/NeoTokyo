61205aa2d7fc2ee48e3dc28be9296f5b557535f8
anazks <anazksunil2@gmail.com>
2025-02-10 18:45:48 +0530

    comnted unused vars

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -44,19 +44,19 @@
 
 // export default Home
 
-import React from "react";
-import NavBar from "../components/user/NavBar/NavBar";
-import Quick from "../components/user/QuickPiks/Quick";
-import Gaming from "../components/user/GamingR1/Gaming";
-import FeedBack from "../components/user/FeedBack/FeedBack";
-import AboutUs from "../components/user/AboutUs/AboutUs";
-import Quality from "../components/user/AboutUs/Quality";
-// import Directional from "../components/user/AboutUs/Directional";
-import Footer from "../components/user/Footer/Footer";
-import PriorityOne from "../components/user/PriorityOne/PriorityOne";
-import Blog from "../components/user/Blog/Blog";
-// <<<<<<< supportPage
-import Support from "../components/user/Support/Support";
+// import React from "react";
+// import NavBar from "../components/user/NavBar/NavBar";
+// import Quick from "../components/user/QuickPiks/Quick";
+// import Gaming from "../components/user/GamingR1/Gaming";
+// import FeedBack from "../components/user/FeedBack/FeedBack";
+// import AboutUs from "../components/user/AboutUs/AboutUs";
+// import Quality from "../components/user/AboutUs/Quality";
+// // import Directional from "../components/user/AboutUs/Directional";
+// import Footer from "../components/user/Footer/Footer";
+// import PriorityOne from "../components/user/PriorityOne/PriorityOne";
+// import Blog from "../components/user/Blog/Blog";
+// // <<<<<<< supportPage
+// import Support from "../components/user/Support/Support";
 // =======
 // import Subscription from '../components/user/Subscription/Subscription'
 // >>>>>>> master
